361f6d165280df85a7860c8ca47e46a3cf85189b
liuyingjie-x <1481470356@qq.com>
2019-05-08 22:41:11 +0800

    @author liuyingjie 5.8提交

--- app/src/main/java/com/lyj/git/MainActivity.java
+++ app/src/main/java/com/lyj/git/MainActivity.java
@@ -2,12 +2,23 @@ package com.lyj.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+/**
+ * 测试git类
+ */
 public class MainActivity extends AppCompatActivity {
+    private final String mStrOne = "master-5.8";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        TextView tvContent = findViewById(R.id.tv_content);
+        tvContent.setText(mStrOne);
     }
 }
